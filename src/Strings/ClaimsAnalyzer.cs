a3a5ac10b25822743173da71f695fdde0da6f5f4
Snorre Stenerud <snorre.stenerud@gmail.com>
2022-12-14 16:15:32 +0100

    comment

--- src/Strings/ClaimsAnalyzer.cs
+++ src/Strings/ClaimsAnalyzer.cs
@@ -7,6 +7,7 @@ public static class ClaimsAnalyser
     public static bool HasNightPrivileges(ClaimsPrincipal user)
     {
         // this code is not correct, but I don't want you to fix it. Write tests that assume that this code is correct
+        // you could imagine this having a dependency to AD or some http-api, which would make it "impossible" to test
         if (!user.Identities.FirstOrDefault()!.IsAuthenticated)
         {
             return false;
